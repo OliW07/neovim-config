03be8438fd64ac8c3d099b1975d0c2bc6cd8135e
Oliver White <oliverjwhite07@gmai.com>
2026-03-30 21:56:40 +1300

    unbreak treesitter

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -28,7 +28,6 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'cpp' },
       auto_install = true,
