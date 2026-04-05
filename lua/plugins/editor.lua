c0942a6b2052df8a76ebefea240404cffb8023f7
Oliver White <oliverjwhite07@gmail.com>
2026-04-05 19:12:02 +1200

    Add missing treesitter parsers for common languages (python, javascript, typescript, css, json, yaml, go, rust)

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -29,7 +29,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'cpp' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'cpp', 'python', 'javascript', 'typescript', 'css', 'json', 'yaml', 'go', 'rust' },
       auto_install = true,
       highlight = { enable = true, additional_vim_regex_highlighting = { 'ruby' } },
       indent = { enable = true, disable = { 'ruby' } },
